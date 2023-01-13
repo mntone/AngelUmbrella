7b4dbe0e9b2942179529f5c3115b5b340b4f95e7
mntone <mntone@outlook.jp>
2023-01-13 15:54:40 +0900

    Adds StringIsPresentToVisibilityConverter.

---
+++ src/Converters/StringIsPresentToVisibilityConverter.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "StringIsPresentToVisibilityConverter.h"
+#if __has_include("Converters/StringIsPresentToVisibilityConverter.g.cpp")
+#include "Converters/StringIsPresentToVisibilityConverter.g.cpp"
+#endif
+
+namespace winrt {
+	using namespace ::winrt::Windows::Foundation;
+	using namespace ::winrt::Windows::UI::Xaml::Interop;
+
+	using namespace ::winrt::Microsoft::UI::Xaml;
+}
+
+using namespace winrt::Mntone::AngelUmbrella::Converters::implementation;
+
+StringIsPresentToVisibilityConverter::StringIsPresentToVisibilityConverter() noexcept {
+}
+
+winrt::IInspectable StringIsPresentToVisibilityConverter::Convert(IInspectable const& value, TypeName const& targetType, IInspectable const& /*parameter*/, hstring const& /*language*/) const {
+	WINRT_ASSERT(xaml_typename<Visibility>() == targetType);
+
+	std::optional<hstring> string { value.try_as<hstring>() };
+	return box_value(string.has_value() && !string.value().empty() ? Visibility::Visible : Visibility::Collapsed);
+}
+
+winrt::IInspectable StringIsPresentToVisibilityConverter::ConvertBack(IInspectable const& /*value*/, TypeName const& /*targetType*/, IInspectable const& /*parameter*/, hstring const& /*language*/) const {
+	throw hresult_not_implemented();
+}
---
+++ test/Converters/StringIsPresentToVisibilityConverterTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Mntone.AngelUmbrella.Tests.Converters
+{
+	[TestClass]
+	public sealed class StringIsPresentToVisibilityConverterTest : StringConverterTestSupport<StringIsPresentToVisibilityConverter, Visibility>
+	{
+		[TestMethod]
+		public void ConvertNullptr()
+		{
+			var retValue = Convert(null);
+			Assert.AreEqual(Visibility.Collapsed, retValue);
+		}
+
+		[TestMethod]
+		public void ConvertEmpty()
+		{
+			var retValue = Convert("");
+			Assert.AreEqual(Visibility.Collapsed, retValue);
+		}
+
+		[TestMethod]
+		public void ConvertPresent()
+		{
+			var retValue = Convert("string");
+			Assert.AreEqual(Visibility.Visible, retValue);
+		}
+
+		[TestMethod]
+		public void ConvertBackNotImplemented()
+		{
+			Assert.ThrowsException<NotImplementedException>(() => ConvertBackUnsetValue());
+		}
+	}
+}
