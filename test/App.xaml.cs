27290c65070d30d0f7891febeaa499ca12ea5434
mntone <mntone@outlook.jp>
2023-01-13 17:15:34 +0900

    Fixes an issue that some UI tests are broken.

--- test/App.xaml.cs
+++ test/App.xaml.cs
@@ -1,11 +1,15 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using Windows.Graphics;
+using WinRT.Interop;
 
 namespace Mntone.AngelUmbrella.Test
 {
 	public sealed partial class App : Application
 	{
-		//private Window? _window;
+		private Window? _window;
 
 		public App()
 		{
@@ -16,10 +20,14 @@ namespace Mntone.AngelUmbrella.Test
 		{
 			Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.CreateDefaultUI();
 
-			//_window = new MainWindow();
-			//_window.Activate();
+			_window = new MainWindow();
+			var hWnd = WindowNative.GetWindowHandle(_window);
+			var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+			var appWindow = AppWindow.GetFromWindowId(windowId);
+			appWindow.ResizeClient(new SizeInt32(1, 1));
+			appWindow.Show();
 
-			//UITestMethodAttribute.DispatcherQueue = _window.DispatcherQueue;
+			UITestMethodAttribute.DispatcherQueue = _window.DispatcherQueue;
 
 			// Replace back with args.Arguments when https://github.com/microsoft/microsoft-ui-xaml/issues/3368 is fixed
 			Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.Run(Environment.CommandLine);
