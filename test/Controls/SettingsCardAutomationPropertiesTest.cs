b433447b6085501c2fd31c67c1a39924a8534c51
mntone <mntone@outlook.jp>
2023-01-15 21:12:36 +0900

    Adds SettingsCard UIA tests.

---
+++ test/Controls/SettingsCardAutomationPropertiesTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using Mntone.AngelUmbrella.Controls;
+
+namespace Mntone.AngelUmbrella.Tests.Controls
+{
+	[TestClass]
+	public sealed class SettingsCardAutomationPropertiesTest
+	{
+		private readonly Lazy<SettingsCard> _control = new Lazy<SettingsCard>(() =>
+		{
+			var card = new SettingsCard()
+			{
+				Description = nameof(SettingsCard.Description),
+				Header = nameof(SettingsCard.Header),
+			};
+			return card;
+		});
+
+		private SettingsCard Control => _control.Value;
+
+		[UITestMethod]
+		public void Description()
+		{
+			var peer = FrameworkElementAutomationPeer.FromElement(Control);
+			var propValue = peer.GetHelpText();
+			Assert.AreEqual(nameof(SettingsCard.Description), propValue);
+		}
+
+		[UITestMethod]
+		public void Header()
+		{
+			var peer = FrameworkElementAutomationPeer.FromElement(Control);
+			var propValue = peer.GetName();
+			Assert.AreEqual(nameof(SettingsCard.Header), propValue);
+		}
+	}
+}
